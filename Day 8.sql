d30c439aeeddc942a33e1fcfc06cf0fb787bc39d
Anuj Kumar Bose <boseanujkumar718@gmail.com>
2025-11-11 23:22:06 +0530

    Add files via upload

---
+++ Day 8.sql
@@ -0,0 +1,61 @@
+SELECT UPPER(name) AS name_upper
+FROM patients;
+
+SELECT CONCAT(name, '-', service) AS patient_info
+FROM patients;
+
+SELECT name,
+LENGTH(name) AS name_length
+FROM patients
+WHERE LENGTH(name) > 15;
+
+SELECT SUBSTRING(name, 1, 3) AS name_abbr
+FROM patients;
+
+SELECT REPLACE(service, 'emergency', 'ER') AS service_abbr
+FROM patients;
+
+SELECT name,
+CASE 
+	WHEN LENGTH(name) > 10 
+	THEN SUBSTRING(name, 1, 4) || '...'
+	ELSE name
+	END AS display_name
+FROM patients;
+
+-- Practice Questions:
+-- 1. Convert all patient names to uppercase.
+SELECT UPPER(name) AS patient_name
+FROM patients;
+
+-- 2. Find the length of each staff member's name.
+SELECT staff_name,
+LENGTH(staff_name) AS staff_name_length
+FROM staff;
+
+--3. Concatenate staff_id and staff_name with a hyphen separator.
+SELECT CONCAT(staff_id, '-', staff_name) AS staff_info
+FROM staff;
+
+-- Daily Challenge:
+
+-- Question: Create a patient summary that shows patient_id, full name in uppercase, 
+-- service in lowercase, age category (if age >= 65 then 'Senior', if age >= 18 then 'Adult', else 'Minor'), 
+-- and name length. Only show patients whose name length is greater than 10 characters.
+
+SELECT
+patient_id,
+UPPER(name) AS patient_name,
+LOWER(service) AS service,
+age,
+CASE 
+	WHEN age >= 65
+	THEN 'Senior'
+
+	WHEN age >= 18
+	THEN 'Adult'
+
+	ELSE 'Minor'
+	END AS age_group
+FROM patients
+WHERE LENGTH(name) > 10;
